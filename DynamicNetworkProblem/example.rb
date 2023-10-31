a38ef72a8a119af3bd944b0e8435d1a3f597e197
dennyKML <denis.kimlyk.of@gmail.com>
2023-10-31 21:53:06 +0200

    Update example.rb

--- DynamicNetworkProblem/example.rb
+++ DynamicNetworkProblem/example.rb
@@ -8,6 +8,8 @@ balancer = LoadBalancer.new([100, 150, 200])
 # Створимо вузли з різними навантаженнями
 nodes = [10, 20, 30, 40, 50, 60, 70, 80, 90].map { |load| Node.new(load) }
 
+puts "The load of nodes trying to connect: #{nodes.map(&:load)}\n\n"
+
 # Виведемо навантаження серверів до розподілу вузлів і після
 puts "Before distribution:"
 balancer.print_loads_and_nodes
